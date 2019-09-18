c3a1904c7761f86621799f600935f8c698bec630
Katherine <ktrnrzhkn@gmail.com>
2019-09-19 00:56:57 +0500

    Delete unwanted whitespace at the end of lines

--- job_result.sql
+++ job_result.sql
@@ -1,37 +1,37 @@
-CREATE DATABASE IF NOT EXISTS job_result CHARACTER SET utf8 COLLATE utf8_bin; 
-GRANT ALL PRIVILEGES ON job_result.* TO 'kthrn'@'localhost'; 
-flush PRIVILEGES; 
+CREATE DATABASE IF NOT EXISTS job_result CHARACTER SET utf8 COLLATE utf8_bin;
+GRANT ALL PRIVILEGES ON job_result.* TO 'kthrn'@'localhost';
+flush PRIVILEGES;
 
-# 
+#
 DROP TABLE IF EXISTS tbl_list;
 DROP TABLE IF EXISTS tbl_celebration;
 
 #
 CREATE TABLE IF NOT EXISTS tbl_list (
-        id              INT(11)     NOT NULL AUTO_INCREMENT, 
+        id              INT(11)     NOT NULL AUTO_INCREMENT,
         name_patronymic VARCHAR(30) NOT NULL,
         lastname        VARCHAR(30) NOT NULL,
         gender          TINYINT(1) NOT NULL,
         position        VARCHAR(50) NOT NULL,
         date_of_birth   DATE NOT NULL,
         PRIMARY KEY (`id`),
-        UNIQUE KEY `position` (`position`)  
-) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+        UNIQUE KEY `position` (`position`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #    
 CREATE TABLE IF NOT EXISTS  tbl_celebration (
-        id             INT(11)     NOT NULL AUTO_INCREMENT, 
+        id             INT(11)     NOT NULL AUTO_INCREMENT,
         celebration    VARCHAR(200) NOT NULL,
         gender      TINYINT(1) NOT NULL,
         text    VARCHAR(500) NOT NULL,
-        PRIMARY KEY (`id`)    
+        PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #
-INSERT INTO `tbl_list` (`name_patronymic`,`lastname`,`gender`,`position`,`date_of_birth`) VALUES ('Виктор Филиппович','Рашников',0,'Председатель совета директоров ММК','1948-10-13'); 
-INSERT INTO `tbl_list` (`name_patronymic`,`lastname`,`gender`,`position`,`date_of_birth`) VALUES ('Павел Владимирович','Шиляев',0,'Генеральный директор ММК','1970-07-03'); 
-INSERT INTO `tbl_list` (`name_patronymic`,`lastname`,`gender`,`position`,`date_of_birth`) VALUES ('Ольга Викторовна','Рашникова',1,'Неисполнительный директор ММК','1977-02-06'); 
-INSERT INTO `tbl_list` (`name_patronymic`,`lastname`,`gender`,`position`,`date_of_birth`) VALUES ('Сергей Валентинович','Кривощеков',0,'Директор по корпоративным вопросам ММК','1961-02-16'); 
+INSERT INTO `tbl_list` (`name_patronymic`,`lastname`,`gender`,`position`,`date_of_birth`) VALUES ('Виктор Филиппович','Рашников',0,'Председатель совета директоров ММК','1948-10-13');
+INSERT INTO `tbl_list` (`name_patronymic`,`lastname`,`gender`,`position`,`date_of_birth`) VALUES ('Павел Владимирович','Шиляев',0,'Генеральный директор ММК','1970-07-03');
+INSERT INTO `tbl_list` (`name_patronymic`,`lastname`,`gender`,`position`,`date_of_birth`) VALUES ('Ольга Викторовна','Рашникова',1,'Неисполнительный директор ММК','1977-02-06');
+INSERT INTO `tbl_list` (`name_patronymic`,`lastname`,`gender`,`position`,`date_of_birth`) VALUES ('Сергей Валентинович','Кривощеков',0,'Директор по корпоративным вопросам ММК','1961-02-16');
 
 #
 INSERT INTO `tbl_celebration` (`celebration`,`gender`,`text`)  VALUES ("День рождения","0","Поздравляю Вас с днем рождения!");
